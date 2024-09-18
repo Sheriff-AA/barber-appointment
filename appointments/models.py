d29db990104d570122181d0a57fe7ef45df35c96
Sherif Atitebi <sherifatitebi@gmail.com>
2024-09-18 18:45:26 +0100

    models implementation and appointment app created

---
+++ appointments/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from datetime import timedelta
+
+
+class User(AbstractUser):
+    pass
+
+
+class TimeSlot(models.Model):
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    class Meta:
+        unique_together = ('date', 'start_time', 'end_time')
+
+    def __str__(self):
+        return f"{self.date} | {self.start_time} - {self.end_time}"
+    
+
+class Appointment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    slot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE, related_name='appointments')
+    booked_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'slot')
+
+    def __str__(self):
+        return f"Appointment with {self.user.username} on {self.slot}"
