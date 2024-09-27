0a0e7fd8186e266aa316ba9060bb4603793af745
Sherif Atitebi <sherifatitebi@gmail.com>
2024-09-28 00:49:55 +0100

    just a view, project structure ready

--- appointments/views.py
+++ appointments/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
 
 # Create your views here.
 def index(request):
     return render(request, 'landing_page.html')
+
+
+class AppointmentsListView(generic.ListView):
+    template_name = "appointments/appointments_list"
