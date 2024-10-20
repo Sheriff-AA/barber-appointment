20176adaa4f79fef9dc5190f959453fe9d5a4ff5
Sherif Atitebi <sherifatitebi@gmail.com>
2024-10-20 03:59:51 +0100

    barber profile, rating, comment

--- appointments/models.py
+++ appointments/models.py
@@ -2,15 +2,25 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from datetime import timedelta
 
+from profiles.models import UserProfile
+from barbers.models import Barber
+
+RATINGS_CHOICES = (
+    (1, "Bad!"),
+    (2, "Meh"),
+    (3, "Decent"),
+    (4, "Barber cooked"),
+    (5, "Give this man your money!")
+)
 
 class User(AbstractUser):
     pass
-
 
 class TimeSlot(models.Model):
     date = models.DateField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+    barber = models.ForeignKey(Barber, on_delete=models.CASCADE, null=True, blank=True)
 
     class Meta:
         unique_together = ('date', 'start_time', 'end_time')
@@ -20,12 +30,18 @@ class TimeSlot(models.Model):
     
 
 class Appointment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True, blank=True)
     slot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE, related_name='appointments')
     booked_at = models.DateTimeField(auto_now_add=True)
+    is_accepted = models.BooleanField(default=False)
 
     class Meta:
-        unique_together = ('user', 'slot')
+        unique_together = ('profile', 'slot')
 
     def __str__(self):
         return f"Appointment with {self.user.username} on {self.slot}"
+    
+
+class Rating(models.Model):
+    value = models.IntegerField(choices=RATINGS_CHOICES, default=None, null=True, blank=True)
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
--- barbers/models.py
+++ barbers/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
-from profiles.models import ProfileType
+from profiles.models import UserProfile
+
+class Barber(models.Model):
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=220)
+
+
+class Comment(models.Model):
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    barber = models.ForeignKey(Barber, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
 
