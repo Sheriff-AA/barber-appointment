d6d4b9177e2b11e89001ae2a0f162b71367f7e52
Sherif <sherifatitebi@gmail.com>
2024-12-29 07:36:16 +0000

    partials for barber timeslots

--- barbers/urls.py
+++ barbers/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import EditBarberProfileView, BarberListView, BarberDetailView, BarberProfileView
+from .views import BarberProfileDetailView, BarberListView, BarberDetailView, BarberProfileUpdateView, BarbersTimeslotListTemplateView
 
 """
 BASE ENDPOINT /barbers
@@ -13,7 +13,8 @@ urlpatterns = [
     path('', BarberListView.as_view(), name='list-barber'),
     path('book/', TemplateView.as_view(template_name='barbers/book_barber_slots.html'), name='book-barber'),
     path('requests/', TemplateView.as_view(template_name='barbers/barber_requests.html'), name='barber-appointment-requests'),
-    path('<slug:slug>/edit/', EditBarberProfileView.as_view(), name='edit-barber-profile'),
+    path('<slug:slug>/edit/', BarberProfileUpdateView.as_view(), name='edit-barber-profile'),
     path('<slug:slug>/details', BarberDetailView.as_view(), name='detail-barber'),
-    path('<slug:slug>/profile', BarberProfileView.as_view(), name='barber-profile'),
+    path('<slug:slug>/profile', BarberProfileDetailView.as_view(), name='barber-profile'),
+    path('<slug:slug>/timeslots', BarbersTimeslotListTemplateView.as_view(), name='barber-timeslots')
 ]
--- barbers/views.py
+++ barbers/views.py
@@ -8,7 +8,8 @@ from .models import Barber
 from profiles.mixins import BarberRequiredMixin
 from appointments.models import TimeSlot, Appointment
 
-class EditBarberProfileView(generic.UpdateView):
+
+class BarberProfileUpdateView(generic.UpdateView):
     template_name = "barbers/barber_profile_edit.html"
     form_class = EditBarberProfileModelForm
 
@@ -52,7 +53,7 @@ class BarberDetailView(generic.DetailView):
         return context
 
 
-class BarberProfileView(BarberRequiredMixin, generic.DetailView):
+class BarberProfileDetailView(BarberRequiredMixin, generic.DetailView):
     template_name = "barbers/barber_profile.html"
     context_object_name = 'barber'
 
@@ -63,7 +64,7 @@ class BarberProfileView(BarberRequiredMixin, generic.DetailView):
         context = super().get_context_data(**kwargs)
         available_slots = TimeSlot.objects.filter(
             barber=self.get_object()).filter(
-            date__gt=now().date()).filter(
+            date__lt=now().date()).filter(
             is_reserved=False).order_by('date', 'start_time')
         appointments = Appointment.objects.filter(slot__barber=self.get_object())
 
@@ -73,9 +74,32 @@ class BarberProfileView(BarberRequiredMixin, generic.DetailView):
         #     grouped_slots[slot.date].append(slot)
         
         context.update({
-            'my_slots': available_slots,
+            'slots': available_slots,
             'requested_appointments': appointments.filter(is_accepted=False),
             'confirmed_appointments': appointments.filter(is_accepted=True)
         })
 
         return context
+    
+
+class BarbersTimeslotListTemplateView(generic.TemplateView):
+    template_name = "barbers/partials/barber_timeslots.html"
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        barber = Barber.objects.get(slug=context['slug'])
+        available_slots = TimeSlot.objects.filter(
+            barber=barber).filter(
+            is_reserved=False).order_by('date', 'start_time')
+        
+        context.update({
+            'slots': available_slots,
+            'barber': barber
+        })
+        
+        if request.user.is_authenticated and request.user.has_perm('profiles.access_to_barber_actions'):
+            context.update({
+                'is_barber': True
+            })
+            
+        return render(request, self.template_name, context)
